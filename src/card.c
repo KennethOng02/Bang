bb1e205ba825f169e1cc3bc17154195d8732d30f
brian098091 <brian098091@gmail.com>
2022-06-14 21:43:21 +0800

    you are my sunshine

--- src/card.c
+++ src/card.c
@@ -87,3 +87,26 @@ void Card_free(Card *this) {
 // 	}
 // 	return 0;
 // }
+
+int play_CARD_BANG(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_MISS(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_GATLING(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_INDIANS(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_PANIC(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_BALOU(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_STAGECOACH(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_FARGO(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_STORE(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_BEER(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_SALOON(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_DUEL(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_BARREL(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_SCOPE(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_MUSTANG(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_VOLCANIC(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_SCHOFIELD(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_REMINGTON(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_CARABINE(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_WINCHEDTER(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_JAIL(Avatar * user, Avatar * target, Game * game, Card * card);
+int play_CARD_DYNAMITE(Avatar * user, Avatar * target, Game * game, Card * card);
